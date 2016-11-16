b372c536e6d15121155a923f7bd7d069d2ef750f
U-DESKTOP-8H0BB6I\terje <terje@eyasys.no>
2016-11-16 06:50:10 +0100

    main class added

---
+++ common/src/main/scala/Common.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by terje on 15.11.2016.
+  */
+class Common {
+  val test = 0
+}
---
+++ web/src/main/scala/Boot.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by terje on 15.11.2016.
+  */
+object Boot {
+  def main(args: Array[String]): Unit = {
+    println("Boot Blowfish")
+  }
+}
