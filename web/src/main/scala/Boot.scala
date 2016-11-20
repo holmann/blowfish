03e3a93c22a43c77d088028ed2a25387336c71a6
U-DESKTOP-8H0BB6I\terje <terje@eyasys.no>
2016-11-20 17:27:09 +0100

    Added initial application.conf

--- web/src/main/scala/Boot.scala
+++ web/src/main/scala/Boot.scala
@@ -1,10 +1,17 @@
 /**
   * Created by terje on 15.11.2016.
   */
+import java.io.File
+
+import com.typesafe.config._
+
 object Boot {
   def main(args: Array[String]): Unit = {
     println("Boot Blowfish")
-
-
+  //  val parsedConfig = ConfigFactory.parseFile(new File("src/main/resources/application.conf"))
+ //   val conf = ConfigFactory.load(parsedConfig)
+    val conf = ConfigFactory.load()
+//
+    println("The answer to everything is " + conf.getString("application.answer"))
   }
 }
